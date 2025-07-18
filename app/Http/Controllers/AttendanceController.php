47547654bcffdbf2d7fa8a90f4fb161e22f3cd43
rizkirenden <rizkirenden@gmail.com>
2025-07-19 01:40:17 +0800

    controller CRUD

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
-    //
+     public function index()
+    {
+
+    }
+    public function create()
+    {
+
+    }
+    public function store()
+    {
+
+    }
+    public function edit()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function destroy()
+    {
+
+    }
 }
--- app/Http/Controllers/AttendanceHistoryController.php
+++ app/Http/Controllers/AttendanceHistoryController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class AttendanceHistoryController extends Controller
 {
-    //
+     public function index()
+    {
+
+    }
+    public function create()
+    {
+
+    }
+    public function store()
+    {
+
+    }
+    public function edit()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function destroy()
+    {
+
+    }
 }
